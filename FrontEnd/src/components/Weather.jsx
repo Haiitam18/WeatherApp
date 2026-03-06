73f42e0ae1eb5dbfc775f8beae8269cf8b30e206
Haitam <haitam47200@gmail.com>
2026-03-06 07:34:47 +0100

    Add vercel speedIsights module

--- FrontEnd/src/components/Weather.jsx
+++ FrontEnd/src/components/Weather.jsx
@@ -16,35 +16,35 @@ const Weather = () => {
     const [weatherData, setWeatherData] = useState(null)
 
     const allIcons = {
-        "01d" : clear_icon,
-        "01n" : clear_icon,
-        "02d" : cloud_icon,
-        "02n" : cloud_icon,
-        "03d" : cloud_icon,
-        "03n" : cloud_icon,
-        "04d" : drizzle_icon,
-        "04n" : drizzle_icon,
-        "09d" : rain_icon,
-        "09n" : rain_icon,
-        "10d" : rain_icon,
-        "10n" : rain_icon,
-        "13d" : snow_icon,
-        "13n" : snow_icon,
+        "01d": clear_icon,
+        "01n": clear_icon,
+        "02d": cloud_icon,
+        "02n": cloud_icon,
+        "03d": cloud_icon,
+        "03n": cloud_icon,
+        "04d": drizzle_icon,
+        "04n": drizzle_icon,
+        "09d": rain_icon,
+        "09n": rain_icon,
+        "10d": rain_icon,
+        "10n": rain_icon,
+        "13d": snow_icon,
+        "13n": snow_icon,
     }
 
     const search = async (city) => {
         if (city === "") {
-            alert("Enter City Name") 
+            alert("Enter City Name")
             return;
         }
 
         setWeatherData(null)
-        try{
+        try {
             const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`
             const response = await fetch(url)
             const data = await response.json()
 
-            if(!response.ok){
+            if (!response.ok) {
                 alert(data.message);
                 setWeatherData(false)
                 return
@@ -54,54 +54,54 @@ const Weather = () => {
             setWeatherData({
                 humidity: data.main.humidity,
                 windSpeed: data.wind.speed,
-                temperature : Math.floor(data.main.temp),
-                location : data.name,
-                icon : icon
+                temperature: Math.floor(data.main.temp),
+                location: data.name,
+                icon: icon
             })
         }
-        catch(error){
+        catch (error) {
             setWeatherData(null);
             console.error("Error in fetching data", error);
         }
-    } 
+    }
 
     useEffect(() => {
         search("Paris")
-    },[] )
+    }, [])
 
-  return (
-    <div className='weather'>
-      <div className='search-bar'>
-        <input ref={inputRef} type="text" placeholder='Search city...' onKeyDown={(e) => {
-    if (e.key === "Enter") {
-      search(inputRef.current.value)
-    }
-  }}/>
-        <img src={search_icon} alt="" onClick={()=> search(inputRef.current.value)}/>
-      </div>
-      {weatherData? <>
-        <img src={weatherData.icon} alt="" className='weather-icon'/>
-        <p className='temperature'>{weatherData.temperature}°C</p>
-        <p className='location'>{weatherData.location}</p>
-        <div className="weather-data">
-            <div className="col">
-                <img src={humidity_icon} alt="" />
-                <div>
-                    <p>{weatherData.humidity}%</p>
-                    <span>Humidity</span>
-                </div>
+    return (
+        <div className='weather'>
+            <div className='search-bar'>
+                <input ref={inputRef} type="text" placeholder='Search city...' onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                        search(inputRef.current.value)
+                    }
+                }} />
+                <img src={search_icon} alt="" onClick={() => search(inputRef.current.value)} />
             </div>
-            <div className="col">
-                <img src={wind_icon} alt="" />
-                <div>
-                    <p>{weatherData.windSpeed} Km/h</p>
-                    <span>Wind Speed</span>
+            {weatherData ? <>
+                <img src={weatherData.icon} alt="" className='weather-icon' />
+                <p className='temperature'>{weatherData.temperature}°C</p>
+                <p className='location'>{weatherData.location}</p>
+                <div className="weather-data">
+                    <div className="col">
+                        <img src={humidity_icon} alt="" />
+                        <div>
+                            <p>{weatherData.humidity}%</p>
+                            <span>Humidity</span>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <img src={wind_icon} alt="" />
+                        <div>
+                            <p>{weatherData.windSpeed} Km/h</p>
+                            <span>Wind Speed</span>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </> : <></>}
         </div>
-        </> : <></>}
-    </div>
-  )
+    )
 }
 
 export default Weather
